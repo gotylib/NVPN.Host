b933152369d09a538b4f98ad1e20a690552b5c68
Gleb Mihalev <mixalev702@mail.ru>
2025-03-11 19:09:33 +0300

    Initial commit

---
+++ NVPN.Host.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace NVPN.Host.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
