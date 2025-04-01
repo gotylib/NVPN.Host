e53431ce73aa6440f56cd9ba09f3d6d0e9a86a1c
Gleb Mihalev <mixalev702@mail.ru>
2025-04-02 01:38:26 +0300

    feat: Добавлен Nlog

--- Infrastructure/Dto/ServerDto.cs
+++ Infrastructure/Dto/ServerDto.cs
@@ -8,6 +8,15 @@ public class ServerDto
     public int Id { get; set; }
     
     /// <summary>
+    /// Админский логин сервера
+    /// </summary>
+    public string Login { get; set; }
+    
+    /// <summary>
+    /// Админский пароль сервера
+    /// </summary>
+    public string Password { get; set; }
+    /// <summary>
     /// Ссылка на сервер к которому привязан пользоваетль.
     /// </summary>
     public string ServerName { get; set; }
--- Infrastructure/Services/ServersControleService.cs
+++ Infrastructure/Services/ServersControleService.cs
@@ -3,6 +3,7 @@ using DAL.Context;
 using DAL.Entities;
 using Infrastructure.Dto;
 using Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Services;
 
@@ -12,10 +13,13 @@ public class ServersControleService : IServersControleService
     
     private readonly VpnDbContext _context;
     
-    public ServersControleService(IMapper mapper, VpnDbContext context)
+    private readonly ILogger<ServersControleService> _logger;
+    
+    public ServersControleService(IMapper mapper, VpnDbContext context, ILogger<ServersControleService> logger)
     {
         _mapper = mapper;
         _context = context;
+        _logger = logger;
     }
     
     public async Task<bool> AddServerAsync(ServerDto server)
@@ -25,10 +29,12 @@ public class ServersControleService : IServersControleService
             var serverEntity = _mapper.Map<Server>(server);
             _context.Servers.Add(serverEntity);
             await _context.SaveChangesAsync();
+            throw new InvalidOperationException("Это тестовое исключение для проверки логирования.");
             return true;
         }
         catch (Exception ex)
         {
+            _logger.LogError(ex.Message);
             return false;
         }
     }
